cca70ddbd8128d9c727e6689120998b0ab0f1673
wesley almeida <78365951+codigoperfeito@users.noreply.github.com>
2021-05-07 14:47:33 -0300

    firt edit
    is hard to me for i is be newbie.

--- Javascript/exercises-3/script.js
+++ Javascript/exercises-3/script.js
@@ -1,1 +1,11 @@
-
+var btn = document.querySelector('tr');
+btn.addEventListener("click", myFunction);
+function myFunction(){
+  this.valor = event.target.value;
+  return valor;
+}
+var valor = ''
+changeSC(valor);
+function changeSC(x){
+  document.querySelector('p').innerHTML = x;
+}
