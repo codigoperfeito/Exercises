4a1f2991804ce1ef18b7382cf0e8f0079165de3d
wesley almeida <78365951+codigoperfeito@users.noreply.github.com>
2021-05-14 18:00:07 -0300

    update 5º ( calc )

--- Javascript/exercises-3/script.js
+++ Javascript/exercises-3/script.js
@@ -1,7 +1,12 @@
 const calculadora = {
   numero : '',
+  fimCalc : '',
   valor(x){
     this.numero += x;    
-  },  
+  },
+  calcular(){    
+    this.fimCalc = eval(this.numero.split('').join(''));
+    this.numero = this.fimCalc;
+  },
 };
-document.querySelector('table').addEventListener('click', function(){ document.querySelector('p').innerHTML = calculadora.numero});
+document.querySelector('table').addEventListener('click', function(){ document.querySelector('input').value = calculadora.numero});
