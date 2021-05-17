7acade50a0b11ceecfa225ba475d2d253e7db9b3
wesley almeida <78365951+codigoperfeito@users.noreply.github.com>
2021-05-17 10:10:34 -0300

    Update script.js

--- Javascript/exercises-3/script.js
+++ Javascript/exercises-3/script.js
@@ -4,9 +4,14 @@ const calculadora = {
   valor(x){
     this.numero += x;    
   },
+  backspace(){
+    var x = parseInt(this.numero.length - 1);
+    var valor = this.numero.substring(0 , x);
+    this.numero = valor;
+  },
   calcular(){    
     this.fimCalc = eval(this.numero.split('').join(''));
     this.numero = this.fimCalc;
   },
 };
-document.querySelector('table').addEventListener('click', function(){ document.querySelector('input').value = calculadora.numero});
+document.querySelector('table').addEventListener('click', function(){ document.querySelector('p').innerHTML = calculadora.numero});
