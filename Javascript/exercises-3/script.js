ebb261e7c8b24933de75ee592cb45734797ac50a
wesley almeida <78365951+codigoperfeito@users.noreply.github.com>
2021-05-21 15:41:09 -0300

    Update script.js

--- Javascript/exercises-3/script.js
+++ Javascript/exercises-3/script.js
@@ -1,41 +1,38 @@
 const calculadora = {
-  numero : '',
-  fimCalc : '',
-  valor(x){
+  numero: "",
+  fimCalc: "",
+  valor(x) {
     this.numero += x;
-    if(this.numero.charCodeAt() >= 48){
-    let z = this.numero.charAt(this.numero.length-2) + this.numero.charAt(this.numero.length-1);
-    if (z == "%%" || z == "++" || z == "--" || z == "**" || z == "//" || z == ".."){
+    var z = this.numero.charAt(this.numero.length-1);
+    var z = z.charCodeAt();
+    var y = this.numero.charAt(this.numero.length-2);
+    var y = y.charCodeAt();
+    if (z >= 48 || y >= 49){
+      //true carai
+    }else {
       let c = parseInt(this.numero.length - 1);
-        let j = this.numero.substring(0 , c);
-        this.numero = j;
-     };
-    }else{
-  let c = parseInt(this.numero.length - 1);
-        let j = this.numero.substring(0 , c);
-        this.numero = j;
-};
+      let j = this.numero.substring(0, c);
+      this.numero = j;
+    }
   },
-  backspace(){
+  backspace() {
     this.numero = this.numero.toString();
-    var x = parseInt(this.numero.length - 2);
-    console.log(x);
-    var valor = this.numero.substring(0 , x);
-    console.log(valor);
+    var x = parseInt(this.numero.length - 1);
+    var valor = this.numero.substring(0, x);
     this.numero = valor;
   },
-  calcular(){    
-    this.fimCalc = eval(this.numero.split('').join(''));   
+  calcular() {
+    this.fimCalc = eval(this.numero.split("").join(""));
+    console.log(this.fimCalc);
     this.numero = this.fimCalc.toString();
-    if (this.numero.length >= 39){
-        this.numero = "erro: valor muito alto";
-        }else{
-          this.numero = parseFloat(this.fimCalc.toFixed(2));
-        };
-  },
+    if (this.numero.length >= 39) {
+      this.numero = "erro: valor muito alto";
+    } else {
+      this.numero = parseFloat(this.fimCalc.toFixed(2)).toPrecision();
+    }
+  }
 };
-document.querySelector('table').addEventListener('click', clickFun);
-function clickFun(){
-document.querySelector('p').innerHTML = calculadora.numero;
-};
-
+document.querySelector("table").addEventListener("click", clickFun);
+function clickFun() {
+  document.querySelector("p").innerHTML = calculadora.numero;
+}
