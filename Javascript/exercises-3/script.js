e3565399111384abb527c3158159fcf0576f75f4
wesley almeida <78365951+codigoperfeito@users.noreply.github.com>
2021-05-22 08:41:09 -0300

    finish code

--- Javascript/exercises-3/script.js
+++ Javascript/exercises-3/script.js
@@ -1,37 +1,39 @@
-const calculadora = {
-  numero: "",
-  fimCalc: "",
-  valor(x) {
-    this.numero += x;
-    var z = this.numero.charAt(this.numero.length-1);
+const calculator = {
+  number: "",
+  finishCalc: "",
+  v(x) {
+    this.number += x;
+    var z = this.number.charAt(this.number.length-1);
     var z = z.charCodeAt();
-    var y = this.numero.charAt(this.numero.length-2);
+    var y = this.number.charAt(this.number.length-2);
     var y = y.charCodeAt();
     if (z >= 48 || y >= 49){
       //true carai
     }else {
-      let c = parseInt(this.numero.length - 1);
-      let j = this.numero.substring(0, c);
-      this.numero = j;
+      let c = parseInt(this.number.length - 1);
+      let j = this.number.substring(0, c);
+      this.number = j;
     }
   },
   backspace() {
-    this.numero = this.numero.toString();
-    var x = parseInt(this.numero.length - 1);
-    var valor = this.numero.substring(0, x);
-    this.numero = valor;
+    this.number = this.number.toString();
+    var x = parseInt(this.number.length - 1);
+    var v = this.number.substring(0, x);
+    this.number = v;
   },
-  calcular() {
-    this.fimCalc = eval(this.numero.split("").join(""));
-    this.numero = this.fimCalc.toString();
-    if (this.numero.length >= 39) {
-      this.numero = "erro: valor muito alto";
+  final() {
+    this.finishCalc = eval(this.number.split("").join(""));
+    this.number = this.finishCalc.toString();
+    console.log(this.number);
+    console.log(this.finishCalc);
+    if (this.number.length >= 39) {
+      this.number = "erro: value big";
     } else {
-      this.numero = parseFloat(this.fimCalc.toFixed(2)).toPrecision();
+      this.number = parseFloat(this.finishCalc.toFixed(2)).toPrecision();
     }
   }
 };
 document.querySelector("table").addEventListener("click", clickFun);
 function clickFun() {
-  document.querySelector("p").innerHTML = calculadora.numero;
+  document.querySelector("p").innerHTML = calculator.number;
 }
