c52eb6fc9dbf1955f973b0f22561e5d26128fbc2
wesley almeida <78365951+codigoperfeito@users.noreply.github.com>
2021-06-08 15:38:21 -0300

    Update script.js

--- Javascript/exercises-9/script.js
+++ Javascript/exercises-9/script.js
@@ -1,11 +1,20 @@
-var buttons = document.querySelectorAll('.botao button');
-buttons.forEach(function(button){
-button.addEventListener('click', function(){
-var value = this.value;
-if(value == 1) {
-console.log(value+' true');
-} else {
-console.log(value+' false');
-}
-})
-})
+var buttons = document.querySelectorAll("button");
+var form1 = document.querySelector('.inform1');
+var form2 = document.querySelector('.inform2')
+console.log(buttons);
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    var value = this.value;
+    if (value == 1) {
+      form1.style.opacity = 1; 
+      form1.style.left = 0;
+      form2.style.opacity = 0;
+      form2.style.left = "510px";
+    } else {
+      form2.style.opacity = 1; 
+      form2.style.left = 0;
+      form1.style.opacity = 0;
+      form1.style.left = "-510px";
+    }
+  });
+});
