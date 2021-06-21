0f01d3f707fdce8284a95e911777abcbfda25737
wesley almeida <78365951+codigoperfeito@users.noreply.github.com>
2021-06-21 13:59:40 -0300

    Update script.js

--- Javascript/exercises-12/script.js
+++ Javascript/exercises-12/script.js
@@ -8,11 +8,9 @@ function zoomImage(imageId, resultId){
   resunt = document.getElementById(resultId);
 
   // criando elemento PSEUDO 
-
   pseudo = document.createElement('div');
 
   // criando class e colocando nome da class no PSEUDO
-
   pseudo.setAttribute('class','pseudo');
 
   // adicionando PSEUDO antes do IMG
@@ -20,29 +18,40 @@ function zoomImage(imageId, resultId){
 
   // calculo para pegar valor de posições para RESULT 
   // de acordo com tamanho de PSEUDO
-
-  zx = img.clientWidth / pseudo.clientWidth;
-  zy = img.clientHeight / pseudo.clientHeight;
+  zx = img.width / pseudo.clientWidth;
+  zy = img.height / pseudo.clientHeight;
 
   // calculando tamanho da BG do result
+  zrw = img.width * zx;
+  zrh = img.height * zy;
 
-  zrw = img.offsetWidth * zx;
-  zrh = img.offsetHeight * zy;
-
-  // adicionando style de RESULT para ficar do tamanho da IMG
-  
-  resunt.style.width = img.offsetWidth + "px";
-  resunt.style.height = img.offsetHeight + "px";
+  // adicionando style de RESULT para ficar do tamanho da IMG  
+  resunt.style.width = img.width + "px";
+  resunt.style.height = img.height + "px";
 
   // Adicionando eventos
+
+  img.addEventListener('mouseover', pseudoVisible);
+  pseudo.addEventListener('mouseover', pseudoVisible);
+
+  img.addEventListener('mouseout', pseudoHidden);
+  pseudo.addEventListener('mouseout', pseudoHidden);
+
   img.addEventListener('mousemove', pseudoMove);
   pseudo.addEventListener('mousemove', pseudoMove);
 
   // funções
+  function pseudoVisible() {   
+    pseudo.style.opacity = 1;
+  };
+  function pseudoHidden() {    
+    pseudo.style.opacity = 0;    
+  };
 
   function pseudoMove(e){
-    // adicionando a função "get..." no pos para acessar valores
 
+    // adicionando a função "get..." no pos para acessar valores
+    
     pos = getCursorPos(e);
     
     // acessando valores com pos
@@ -57,36 +66,49 @@ function zoomImage(imageId, resultId){
     // codigo para ele não ficar atrás ( -X / -Y ) do quadrado da imagem
 
     if (x < 0){
-      x = 0;
+      x = 0;    
+      pseudo.style.opacity = 1;
     }
     
     if (y < 0){
       y = 0;
+      pseudo.style.opacity = 1;
     }
 
     // codigo para ele não ultrapassar o quadrado da imagem (+X / +Y)
 
     if (x > img.offsetWidth - pseudo.offsetWidth){
       x = img.offsetWidth - pseudo.offsetWidth;
+      pseudo.style.opacity = 1;
     }
     if (y > img.offsetHeight - pseudo.offsetHeight){
       y = img.offsetHeight - pseudo.offsetHeight;
+      pseudo.style.opacity = 1;
     }
-    /* fazendo nosso mouse se mover */
+    /* fazendo nosso mouse se mover e adicionando a diferença
+    da dela porque pos.a é calculo da posição da img logo precisa
+    calcular o valor caso tenha scroll para descer */
+
     pseudo.style.left = (pos.a.left + window.pageXOffset) + x + "px";
     pseudo.style.top = (pos.a.top + window.pageYOffset) + y + "px";
+
+    // ajeitando a imagem no RESULT tanto tamanho como posição
 
     result.style.backgroundSize = zrw + 'px ' + zrh + 'px';
     result.style.backgroundImage = "url(" + img.src + ")";
     resunt.style.backgroundPosition = - (x * zx) + "px " + - (y * zy)  + "px";
   }
-  function getCursorPos(){
-    var a, e, x, y;
+  function getCursorPos(e){    
+    // variaveis que serão usadas
+
+    var a, x, y;
 
     //pegando evento window
-    e = window.event;
+
+    e = window.event
 
     // adicionando as propriedades ao A da img
+
     a = img.getBoundingClientRect();
 
     // x e y vão receber posição mouse na pagina menos posição da img
